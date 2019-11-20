7f2ec234ec3b411305adbaf621f4263cfc2d30cc
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-11-20 02:53:21 +0100

    Enhance messages glorioussness

--- DjangoProjects/ngs/views.py
+++ DjangoProjects/ngs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
+    messages.success(request, "You successfully came here !")
     return render(request, "ngs/home.html", locals())
