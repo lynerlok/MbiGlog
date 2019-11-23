addd1d0202eb9a1647c509b0317674b53534ca3e
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-11-23 16:34:08 +0100

    refactor populate + deal with absent taxons

--- DjangoProjects/imagerie/management/commands/populate_imagerie.py
+++ DjangoProjects/imagerie/management/commands/populate_imagerie.py
@@ -9,20 +9,23 @@ from imagerie.models import *
 class Command(BaseCommand):
     args = '<directory_of_images_and_annotation_path>'
     help = 'our help string comes here'
+    taxon_names = ["Regnum", 'Class', 'Subclass', 'Superorder', 'Order', 'Family', 'Genus', 'Species']
+    taxon_ranks = [RankTaxon.objects.get_or_create(name=name) for name in taxon_names]
 
     def add_arguments(self, parser):
         parser.add_argument('dir_path')
 
-    def _populate_db(self, dir_path):
-        REGNUM, _ = RankTaxon.objects.get_or_create(name='Regnum')
-        CLASS, _ = RankTaxon.objects.get_or_create(name='Class')
-        SUBCLASS, _ = RankTaxon.objects.get_or_create(name='Subclass')
-        SUPERORDER, _ = RankTaxon.objects.get_or_create(name='Superorder')
-        ORDER, _ = RankTaxon.objects.get_or_create(name='Order')
-        FAMILY, _ = RankTaxon.objects.get_or_create(name='Family')
-        GENUS, _ = RankTaxon.objects.get_or_create(name='Genus')
-        SPECIES, _ = RankTaxon.objects.get_or_create(name='Species')
+    def _get_or_create_taxon(self, taxons, pos=0, previous=None):
+        if pos == 7:
+            return previous
+        taxon = taxons.find(self.taxon_names[pos]).text
+        if taxon:
+            taxon, _ = Taxon.objects.get_or_create(rank=self.taxon_ranks[pos], name=taxon, sup_taxon=previous)
+            return self._get_or_create_taxon(taxons, pos + 1, previous=taxon)
+        else:
+            return previous
 
+    def _populate_db(self, dir_path):
         for file_path in Path(dir_path).iterdir():
             if file_path.suffix == ".xml":
                 image = file_path.name.replace('xml', 'jpg')
@@ -34,21 +37,12 @@ class Command(BaseCommand):
                 typeImage, _ = TypeImage.objects.get_or_create(name=root.find('Type').text)
                 content, _ = ContentImage.objects.get_or_create(name=root.find('Content').text)
                 taxons = root.find('Taxon')
-                regnum, _ = Taxon.objects.get_or_create(rank=REGNUM, name=taxons.find('Regnum').text, sup_taxon=None)
-                class_, _ = Taxon.objects.get_or_create(rank=CLASS, name=taxons.find('Class').text,
-                                                        sup_taxon=regnum)
-                subclass, _ = Taxon.objects.get_or_create(rank=SUBCLASS, name=taxons.find('Subclass').text,
-                                                          sup_taxon=class_)
-                superorder, _ = Taxon.objects.get_or_create(rank=SUPERORDER, name=taxons.find('Superorder').text,
-                                                            sup_taxon=subclass)
-                order, _ = Taxon.objects.get_or_create(rank=ORDER, name=taxons.find('Order').text,
-                                                       sup_taxon=superorder)
-                family, _ = Taxon.objects.get_or_create(rank=FAMILY, name=taxons.find('Family').text, sup_taxon=order)
-                genus, _ = Taxon.objects.get_or_create(rank=GENUS, name=taxons.find('Genus').text, sup_taxon=family)
-                specie, _ = Specie.objects.get_or_create(rank=SPECIES, name=taxons.find('Species').text,
+                genus = self._get_or_create_taxon(taxons, pos=0, previous=None)
+                specie, _ = Specie.objects.get_or_create(rank=self.taxon_ranks[7], name=root.find('ClassId').text,
                                                          sup_taxon=genus,
                                                          vernacular_name=root.find('VernacularNames').text,
                                                          latin_name=root.find('ClassId').text)
+
                 gtimage, _ = GroundTruthImage.objects.get_or_create(specie=specie, content=content, type=typeImage,
                                                                     image=image_path)
                 print(image, 'saved')
