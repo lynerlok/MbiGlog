d8e91fae6a6a19b56693db4ec7f46c54e6db33b6
Maxime Lecomte <maxime.lecomte06@free.fr>
2019-12-25 17:23:04 +0100

    add orga/pwyList/pwyID/ReactionList

--- DjangoProjects/metabo/templates/metabo/parserPwyGene.js
+++ DjangoProjects/metabo/templates/metabo/parserPwyGene.js
@@ -1,31 +1,39 @@
-/*
-PARSER XML
-04/11/2019
-ANDRÉ Charlotte
-GALLARDO Jean Clément
-LECOMTE Maxime
-LAPORTE Antoine
-MERCHADOU Kévin
+//
+// PARSER XML
+// 04/11/2019
+// ANDRÉ Charlotte
+// GALLARDO Jean Clément
+// LECOMTE Maxime
+// LAPORTE Antoine
+// MERCHADOU Kévin
 
-*/
+
 
 const struct = (groups) => {
 	console.log(groups);
+	let orgid = "";
+	let gene_Id = [];
 	let pwy_ID = {
-		"GeneID": [],
-		"ReactionList" : []
+		orgid : [],
+		"pwyID" : [],// contiendra la liste pwy pour un gene ID donné
+		"pwyList" : [],
+		"ReactionList" : [] // contiendra la liste des reaction pour un pwy en cle
 	};
+
 	groups.forEach(cur =>
+		(cur.textContent.match(/[A-Z]*:[A-Z]*[0-9]*/g)) ? pwy_ID.orgid.push(cur.textContent.match(/[A-Z]*:[A-Z]*[0-9]*/g)) :
 		(cur.attributes[0].name === 'ID') ?
-		pwy_ID.GeneID.push(cur.attributes[2].nodeValue) :
-		pwy_ID.ReactionList.push(cur.attributes[2].nodeValue)); // recupere aussi les pathway ressource
+		pwy_ID.pwyID.push(cur.attributes[2].nodeValue) :
+		(cur.attributes.length === 4) ?
+		pwy_ID.pwyList.push(cur.attributes[2].nodeValue) :
+		pwy_ID.ReactionList.push(cur.attributes[2].nodeValue)); // attention il a rajoute des pathway ressources comment XYLCAT-PWY
 	return pwy_ID;
 }
 
 const parseXML = (text) => {
 	let parser = new DOMParser();
 	let doc = parser.parseFromString(text, "application/xml");
-	let secStruct = struct(doc.querySelectorAll("Pathway,Reaction")); // balise a recuperer
+	let secStruct = struct(doc.querySelectorAll("Pathway,query,Reaction") );// balise a recuperer
 	console.log(secStruct);
 }
 
