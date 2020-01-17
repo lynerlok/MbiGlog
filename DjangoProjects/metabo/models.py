47b47fc5e9faa6a3b1cfd15f2b60a24a844ddf19
Maxime Lecomte <maxime.lecomte06@free.fr>
2020-01-17 18:30:59 +0100

    orga

--- DjangoProjects/metabo/models.py
+++ DjangoProjects/metabo/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 import xml.etree.ElementTree as ET
 import requests
+import io
+import re
 
 # Create your models here.
 
 
 class Pathway(models.Model):
+    print('\n ------------------------------------ PATHWAY CLASS ----------------------------------- \n')
     name = models.CharField(max_length=64)
     geneList = models.TextField()
     enzyList = models.TextField()
@@ -13,32 +16,42 @@ class Pathway(models.Model):
 
 
 class Gene(models.Model):
+    print('\n ------------------------------------ GENE CLASS -------------------------------------- \n')
+
     id_gene = models.CharField(max_length=20) ## todo recupere common-name in  gene of pwy pour rentrer id uniprot
-    # gene = models.ManyToManyField(Pathway)
-    #
-    # response = requests.get('https://websvc.biocyc.org/apixml',
-    #                         {'fn': 'genes-of-pathway', 'id':'ARA:PWY-82'})
-    # root = ET.fromstring(response.content)
-    #
-    # for element in root.iter('Gene'):
-    #     if 'ID' in element.attrib:
-    #         print(element.attrib['frameid'])
+    gene = models.ManyToManyField(Pathway)
+
+    def requestFromBiocyc(self):
+        response = requests.get('https://websvc.biocyc.org/apixml',
+                                {'fn': 'genes-of-pathway', 'id':'ARA:PWY-82'})
+        root = ET.fromstring(response.content)
+        genePwyList = []
+        for element in root.iter('Gene'):
+            if 'ID' in element.attrib:
+                print('Genes of pathway : '+element.attrib['frameid'])
+                genePwyList.push(element.attrib['frameid'])
+                return genePwyList
+
 
 class Enzyme(models.Model):
+    print('\n ------------------------------------ ENZYME CLASS ------------------------------------- \n')
     name = models.TextField()
     # cofactor_in = models.ManyToManyField(CoFactor)
     # cofactor_out = models.ManyToManyField(CoFactor)
-    # requestURL = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&gene=AT5G52560&organism=Arabidopsis%20thaliana&taxid=3702"
-    # r = requests.get(requestURL, headers={"Accept": "application/xml"})
-    # responseBody = r.text
-    # buf = io.StringIO(responseBody)
-    # line = buf.readline()
-    # cofactors = re.findall(r'<name>\w+\([0-9]*[\+|\-]\)',line)
-    # enzName = re.findall(r'<fullName>[A-Z]+\-*\_*[a-z]*\s*[a-z]*',line)
-    # print(enzName)
-    # print(cofactors)
+
+    def requestFromUniProt(self):
+        requestURL = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&gene=AT5G52560&organism=Arabidopsis%20thaliana&taxid=3702"
+        r = requests.get(requestURL, headers={"Accept": "application/xml"})
+        responseBody = r.text
+        buf = io.StringIO(responseBody)
+        line = buf.readline()
+        cofactors = re.findall(r'<name>\w+\([0-9]*[\+|\-]\)',line)
+        enzName = re.findall(r'<fullName>[A-Z]+\-*\_*[a-z]*\s*[a-z]*',line)
+        return enzName,cofactors
+
 
 class CoFactor(models.Model):
+    print('\n ------------------------------------ COFACTOR CLASS ----------------------------------- \n')
     # cofactor_in = models.TextField()
     # cofactor_out = models.TextField()
     pass
@@ -46,43 +59,54 @@ class CoFactor(models.Model):
 
 
 class Reaction(models.Model):
+    print('\n ------------------------------------ REACTION CLASS ----------------------------------- \n')
+
     name = models.TextField()
 
     # for metabolite in self.metabolite_set:   ## va chercher les metabolites pour 1 reaction
 
 
-    # def request_from_biocyc(self):            pathways-of-gene
-    #     response = requests.get('https://websvc.biocyc.org/apixml',
-    #                             {'fn': 'pathways-of-gene', 'id': 'ARA:AT5G52560'})
-    #     root = ET.fromstring(response.content)
-    #
-    #     for element in root.iter('Reaction'):
-    #         print(element.attrib['frameid'])
-    #     return render(request, "metabo/home.html", locals())
+    def requestFromBiocyc(self):
+        response = requests.get('https://websvc.biocyc.org/apixml',{'fn': 'pathways-of-gene', 'id': 'ARA:AT5G52560'})
+        root = ET.fromstring(response.content)
+        pwyGeneList = []
+        for element in root.iter('Reaction'):
+            pwyGeneList.push(element.attrib['frameid'])
+            return pwyGeneList
+
+        # return render(request, "metabo/home.html", locals())
 
 
 class Component(models.Model):
+    print('\n ------------------------------------ COMPONENT CLASS ---------------------------------- \n')
+
     name = models.CharField(max_length=50)
-    # pwyList = models.ManyToManyField(Pathway)
-    # requestURL = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&protein=Pyruvate%20kinase&taxid=3702"
-    # r = requests.get(requestURL, headers={"Accept": "application/xml"})
-    # responseBody = r.text
-    # buf = io.StringIO(responseBody)
-    # line = buf.readline()
-    # location = re.findall(r'C:[a-z]*\s*[a-z]*',line)
-    # pathway = re.findall(r'\"UniPathway\"\s*id=\"UPA00109\"',line)
-    # print(location)
-    # print(pathway)
+    pwyList = models.ManyToManyField(Pathway)
+
+    def requestFromUniProt(self):
+        requestURL = "https://www.ebi.ac.uk/proteins/api/proteins?offset=0&size=100&protein=Pyruvate%20kinase&taxid=3702"
+        r = requests.get(requestURL, headers={"Accept": "application/xml"})
+        responseBody = r.text
+        buf = io.StringIO(responseBody)
+        line = buf.readline()
+        location = re.findall(r'C:[a-z]*\s*[a-z]*',line)
+        pathway = re.findall(r'\"UniPathway\"\s*id=\"UPA00109\"',line)
+        return location,pathway
 
 class Metabolite(models.Model):
-    name = models.CharField(max_length=50)
-    # reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE) ## todo manytomany
-    # #
-    # def metabo(self):
-    #     response = requests.get('https://websvc.biocyc.org/apixml', {'fn': 'substrates-of-reaction', 'id': 'ARA:RXN-3'})
-    #     root = ET.fromstring(response.content)
+    print('\n ------------------------------------ METABOLITE CLASS --------------------------------- \n')
 
-    #     for element in root.iter('common-name'):
-    #         print(element.text)
+    name = models.CharField(max_length=50)
+    reaction = models.ForeignKey(Reaction, on_delete=models.CASCADE) ## todo manytomany
+    #
+    def metabo(self):
+        response = requests.get('https://websvc.biocyc.org/apixml', {'fn': 'substrates-of-reaction', 'id': 'ARA:RXN-3'})
+        root = ET.fromstring(response.content)
+
+        subsReact = []
+        for element in root.iter('common-name'):
+            subsReact.push(element.text)
+            return subsReact
+
 
 
