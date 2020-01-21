ac7d5cf11a37c9174000dbbf96e7e97fc4f90032
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2020-01-21 22:45:11 +0100

    Basic modifications of basic pages

--- DjangoProjects/proteo/urls.py
+++ DjangoProjects/proteo/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="prt_home"),
     path('1D/', views.Onedimension, name="one_dimension"),
+    path('2D/', views.Twodimension, name="two_dimension"),
+    path('Pred3D/', views.Pred3Ddimension, name="Predthree_dimension"),
+    path('3D/', views.Threedimension, name="three_dimension"),
 ]
