807b23495e49cd87277437348ae484de90f44338
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2020-01-24 19:31:39 +0100

    Test Alexnet

---
+++ DjangoProjects/imagerie/management/commands/test_alexnet.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from imagerie.models import *
+from imagerie.tf_models.AlexNet import AlexNet
+
+
+class Command(BaseCommand):
+    args = '<directory_of_images_and_annotation_path>'
+    help = 'our help string comes here'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        self.test()
+
+    def test(self):
+        a = AlexNet()
+        images = GroundTruthImage.objects.filter(
+            Q(specie__name__contains='Rhamnus') | Q(specie__name__contains='Euphorbia'))
+        a.train(images)
+        print(a.accuracy)
