6b3a52b9fb56694534b03b1291f76a480ef3d8f2
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2020-01-26 14:01:43 +0100

    default preprocess

--- DjangoProjects/imagerie/models.py
+++ DjangoProjects/imagerie/models.py
@@ -96,11 +96,12 @@ class Image(models.Model):
 
         img = imageio.imread(self.image.path, pilmode='RGB')
         img = np.array(PImage.fromarray(img).resize((224, 224))).astype(np.float32)
-        img[:, :, 0] -= 123.68
-        img[:, :, 1] -= 116.779
-        img[:, :, 2] -= 103.939
+        # img[:, :, 0] -= 123.68
+        # img[:, :, 1] -= 116.779
+        # img[:, :, 2] -= 103.939
         # img[:, :, [0, 1, 2]] = img[:, :, [2, 1, 0]]
-        img = img.transpose((2, 0, 1))
+        # img = img.transpose((2, 0, 1))
+
         return img
 
 
