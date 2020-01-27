cd0b2198e246dc506cd4bcb2b6cf474a51678597
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2020-01-27 18:24:44 +0100

    reset settings

--- DjangoProjects/glogServer/settings.py
+++ DjangoProjects/glogServer/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'glogServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
+#
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'DevDatabase.db',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': get_env_variable('DATABASE_NAME'),
+       'USER': get_env_variable('DATABASE_USER'),
+       'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+	'HOST': 'glog.dust-of-the-universe.com',
+	'PORT': '5432',
+   }
 }
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': get_env_variable('DATABASE_NAME'),
-#        'USER': get_env_variable('DATABASE_USER'),
-#        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-#	'HOST': 'glog.dust-of-the-universe.com',
-#	'PORT': '5432',
-#    }
-#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
