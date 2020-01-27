cd21a6fffb7c5f5f5c35fb7806a778e5cfe5732d
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2020-01-27 18:48:09 +0100

    finished save 5 best predictions only

--- DjangoProjects/glogServer/settings.py
+++ DjangoProjects/glogServer/settings.py
@@ -140,7 +140,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = "/media/Datas/Pictures"
 MEDIA_URL = "/media/"
 
 # Static files (CSS, JavaScript, Images)
