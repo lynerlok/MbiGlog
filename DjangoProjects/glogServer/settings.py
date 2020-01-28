11d4254465a47952dccd89020876b77c73942fb6
lynerlok <univ_apolin@protonmail.com>
2020-01-28 19:24:42 +0100

    Add docker files and modify settings

--- DjangoProjects/glogServer/settings.py
+++ DjangoProjects/glogServer/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 
 MESSAGE_LEVEL = 0
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'glog.dust-of-the-universe.com']
 
 # Application definition
 
---
+++ buildImage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t glogserver:1.0 .
