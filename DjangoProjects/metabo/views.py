ffe384e1b96e6132630135e87635342b2b38ba2a
Maxime Lecomte <maxime.lecomte06@free.fr>
2020-01-31 17:27:29 +0100

    boutton + tentative integration cytoscapejs django

--- DjangoProjects/metabo/views.py
+++ DjangoProjects/metabo/views.py
@@ -4,9 +4,15 @@ import sys
 
 # Create your views here.
 def home(request):
-    geneList = ['AT5G52560','AT4G16130']#'GQT-5255'
-    for i in geneList:
-        gene, _ = Gene.objects.get_or_create(id_gene=i)
-        gene.get_or_create_pathways()
-        Enzyme.create_enzyme_metabolite()
+    # geneList = ['AT5G52560','AT4G16130']#'GQT-5255'
+    # for i in geneList:
+    #     gene, _ = Gene.objects.get_or_create(id_gene=i)
+    #     gene.get_or_create_pathways()
+    #     Enzyme.create_enzyme_metabolite()
+
     return render(request, "metabo/home.html", locals())
+
+
+def graph(request):
+
+    return render(request, "metabo/graph.html", locals())
