02aa1709839981d61efb51f4b032addc1bb89855
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2020-02-02 12:27:50 +0100

    Good length of last layer

--- DjangoProjects/imagerie/models.py
+++ DjangoProjects/imagerie/models.py
@@ -342,8 +342,7 @@ class AlexNet(CNN):
         self.nn_model.add(Dropout(0.4))
 
         # Output Layer
-        # self.nn_model.add(Dense(len(self.classes.all())))
-        self.nn_model.add(Dense(36))
+        self.nn_model.add(Dense(len(self.classes.all())))
         self.nn_model.add(Activation('softmax'))
 
         # Compile the self.nn_model
