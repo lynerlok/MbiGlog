a570340d036dec2cbf46240af91f08e74e8d7ace
AsaLap <asalaporte@gmail.com>
2020-02-05 21:38:45 +0100

    Pipeline for the NGS data, almost ok, problem with the encoding of the file

--- DjangoProjects/metabo/urls.py
+++ DjangoProjects/metabo/urls.py
@@ -5,7 +5,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = [path('ngs/',views.upload_file, name='mtb_ngs'),
                   path('', views.home, name="mtb_home"),
                   path('cytoscape/', views.graph, name="mtb_graph"),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- DjangoProjects/metabo/views.py
+++ DjangoProjects/metabo/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from metabo.models import *
-import sys
+from .forms import NGSForm
+from django.conf import settings
+from pathlib import Path
+from .scripts import correlation as co
+
 
 # Create your views here.
 def home(request):
@@ -13,6 +18,34 @@ def home(request):
     return render(request, "metabo/home.html", locals())
 
 
+def upload_file(request):
+    if request.method == 'POST':
+        form = NGSForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['NGS_data']
+            handle_uploaded_file(file)
+
+            ###Traitement via le script python
+            data = open("media/tmp.txt","r",errors="ignore")
+            dic = co.strToFloat(data)
+            result_correlation = co.corrPearson(dic)
+            correlDict = co.translateMatrix(result_correlation)
+            output = co.meltDict(correlDict, 0.9)
+            co.saveResult(output, "static/tempNGS.txt")
+            return redirect('mtb_graph')
+    else:
+        form = NGSForm()
+    return render(request, 'metabo/ngs.html', {'form': form})
+
+
+def handle_uploaded_file(f):
+    tmp = Path(settings.MEDIA_ROOT) / "tmp.txt"
+    with tmp.open('wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def graph(request):
+    ll = str([[a * b for a in range(50)] for b in range(50)])
 
     return render(request, "metabo/graph.html", locals())
