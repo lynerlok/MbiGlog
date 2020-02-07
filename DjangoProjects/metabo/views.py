08dbecfa0525d0be39fd61cdf37f1e7d988c8626
Maxime Lecomte <maxime.lecomte06@free.fr>
2020-02-07 16:45:49 +0100

    working db and view

--- DjangoProjects/metabo/views.py
+++ DjangoProjects/metabo/views.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from .scripts import correlation as co
 import json
 
+
 # Create your views here.
 def home(request):
     # geneList = ['AT5G52560','AT4G16130']#'GQT-5255'
@@ -48,6 +49,39 @@ def handle_uploaded_file(f):
 
 
 def graph(request):
-    ll = str([[a * b for a in range(50)] for b in range(50)])
+    json_net = {}
 
+    geneList = ['AT5G52560', 'AT4G16130']
+
+    for i in geneList:
+        gene, _ = Gene.objects.get_or_create(id_gene=i)
+        gene.get_or_create_pathways()
+        Metabolite.createMetabolites()
+
+        pwy = Pathway.objects.filter(genes__id_gene=i)
+        for p in pwy:
+            reactions = p.reactions.all()
+            genes=p.genes.all()
+            json_net[p.name] = {}
+            json_net[p.name]['Genes']=[]
+            for g in genes:
+                json_net[p.name]['Genes'].append(g.id_gene)
+            for r in reactions:
+                bilan = r.bilans.all()
+                json_net[p.name][r.name] = {}
+                enzyme = r.enzyme.all()
+
+
+                json_net[p.name][r.name]['enzyme']={}
+                if len(enzyme) > 0:
+                    json_net[p.name][r.name]['enzyme']['name'] = enzyme[0].name
+                    comp = Component.objects.filter(enzymes_id=enzyme[0].name)
+                    if len(comp) > 0:
+                        json_net[p.name][r.name]['enzyme']['component'] = comp[0].name
+                for met in bilan:
+                    json_net[p.name][r.name]['reactif'] = met.reactif
+                    json_net[p.name][r.name]['produit'] = met.produit
+                    json_net[p.name][r.name]['cofactors'] = met.cofactors
+
+    json_string2 = json.dumps(json_net)
     return render(request, "metabo/graph.html", locals())
