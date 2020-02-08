6a2c25d231c81d5d227d054c9f6c8d645c12ee99
jeremybulle <jeremyjp.bulle@gmail.com>
2020-02-08 14:09:46 +0100

    parser multi seq fait

--- DjangoProjects/ngs/parse_fasta.py
+++ DjangoProjects/ngs/parse_fasta.py
@@ -1,22 +1,31 @@
-def parser(fasta_file):
-    with open(fasta_file, 'r') as fasta:
-        txt = fasta.read()
-        txt_tab = txt.split()
-        id_gene = txt_tab[0].split(">")[1]
-        header = txt.split(",")
-        nom_gene = header[0]
-        nom_gene = nom_gene.split()
-        nom_gene = nom_gene[1:]
-        nom_gene = " ".join(nom_gene)
-        sequence = txt.split("\n")[1:]
-        sequence = "".join(sequence)
-        return {"id_gene" : id_gene, "nom_gene": nom_gene, "sequence": sequence}
+def parser(pathway_file):
+    list_seq = []
+    with open(pathway_file, 'r') as file:
+        txt = file.read()
+        raw_fasta_list = txt.split(">")
+        raw_fasta_list.remove("")
+        for seq in raw_fasta_list:
+            seq_dict = {}
+            id = seq.split()[0]
+            name = seq.split("\n")[0].split(" ")[1:]
+            name = " ".join(name)
+            seq = seq.split("\n")[1:]
+            seq = "".join(seq)
+            seq_dict['id'] = id
+            seq_dict['name'] = name
+            seq_dict['sequence'] = seq
+            list_seq.append(seq_dict)
+    return list_seq
+
 
 if __name__ == "__main__":
     import os
-    dict = parser("./fasta/arabi.fasta")
-    print( "id : " , dict['id_gene'])
-    print("nom : " , dict['nom_gene'])
-    print("sequence : \n",dict['sequence'])
+    #dict = parser("./arabi.fasta")
+    #print( "id : " , dict['id_gene'])
+    #print("nom : " , dict['nom_gene'])
+    #print("sequence : \n",dict['sequence'])
+
+    
+    
 
 
