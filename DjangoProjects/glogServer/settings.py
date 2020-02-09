fc81ae4b14e04b1526953e746d15dea037aea5dd
lynerlok <univ_apolin@protonmail.com>
2020-02-09 19:07:49 +0100

    Media root correction

--- DjangoProjects/glogServer/settings.py
+++ DjangoProjects/glogServer/settings.py
@@ -138,7 +138,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
