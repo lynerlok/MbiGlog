11c65aed1c8554da489fa10f3b31d9e2b4eb6c48
lynerlok <univ_apolin@protonmail.com>
2020-02-09 17:32:25 +0100

    Some update

--- startServer.sh
+++ startServer.sh
@@ -7,4 +7,4 @@ workon PythonVenv
 # Django server in development mode
 
 # DjangoProjects/manage.py runserver <YOUR IP or 127.0.0.1>:<PORT>
-python DjangoProjects/manage.py runserver 192.168.0.29:8080
+python DjangoProjects/manage.py runserver 192.168.0.29:8081
