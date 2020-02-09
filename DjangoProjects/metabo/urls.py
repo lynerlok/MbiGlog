1b21bff783acd78f9993536db7ec171753118dc4
AsaLap <asalaporte@gmail.com>
2020-02-09 16:15:30 +0100

    Finalization of CSS, gene network and visualization

--- DjangoProjects/metabo/urls.py
+++ DjangoProjects/metabo/urls.py
@@ -8,4 +8,5 @@ from django.conf.urls.static import static
 urlpatterns = [path('ngs_input/', views.upload_ngs_file, name='mtb_ngs'),
                   path('', views.home, name="mtb_home"),
                   path('cytoscape/', views.graph, name="mtb_graph"),
+                path('visualize/', views.visualize, name="mtb_visu")
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
