ef24cdcab4e73582f19947e73d2f43882867a3b2
Eliot Ragueneau <orkeliot@gmail.com>
2020-02-10 09:07:58 +0100

    Not testing SL

--- DjangoProjects/imagerie/management/commands/test_alexnet.py
+++ DjangoProjects/imagerie/management/commands/test_alexnet.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+
 from imagerie.models import *
 
 
@@ -12,10 +13,10 @@ class Command(BaseCommand):
         self.test()
 
     def test(self):
-        background = BackgroundType.objects.get(name='SheetAsBackground')
-        organ = PlantOrgan.objects.get(name='Leaf')
-        a, _ = AlexNet.objects.get_or_create(name="SL", specialized_background=background, specialized_organ=organ)
-        a.train()
+        # background = BackgroundType.objects.get(name='SheetAsBackground')
+        # organ = PlantOrgan.objects.get(name='Leaf')
+        # a, _ = AlexNet.objects.get_or_create(name="SL", specialized_background=background, specialized_organ=organ)
+        # a.train()
         background = BackgroundType.objects.get(name='NaturalBackground')
         for organ in PlantOrgan.objects.all():
             a, _ = AlexNet.objects.get_or_create(name=f"N{organ.name}", specialized_background=background,
