693c79e796ed33145ddd1ca451b88fe9763415a4
lynerlok <univ_apolin@protonmail.com>
2020-02-10 12:59:01 +0100

    Uncomment in test_alexnet, run all CNNs

--- DjangoProjects/imagerie/management/commands/test_alexnet.py
+++ DjangoProjects/imagerie/management/commands/test_alexnet.py
@@ -13,10 +13,10 @@ class Command(BaseCommand):
         self.test()
 
     def test(self):
-        # background = BackgroundType.objects.get(name='SheetAsBackground')
-        # organ = PlantOrgan.objects.get(name='Leaf')
-        # a, _ = AlexNet.objects.get_or_create(name="SL", specialized_background=background, specialized_organ=organ)
-        # a.train()
+        background = BackgroundType.objects.get(name='SheetAsBackground')
+        organ = PlantOrgan.objects.get(name='Leaf')
+        a, _ = AlexNet.objects.get_or_create(name="SL", specialized_background=background, specialized_organ=organ)
+        a.train()
         background = BackgroundType.objects.get(name='NaturalBackground')
         for organ in PlantOrgan.objects.all():
             a, _ = AlexNet.objects.get_or_create(name=f"N{organ.name}", specialized_background=background,
